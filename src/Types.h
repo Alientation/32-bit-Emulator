462ec4b859f47dd47801801a3cd3ba8f0bed9bd2
Bill <mcpirate1760@gmail.com>
2023-10-02 11:40:20 -0500

    print binary numbers

--- src/Types.h
+++ src/Types.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <bitset>
 
 using Byte = unsigned char;
 
@@ -16,26 +17,55 @@ using u32 = unsigned int;
 using u64 = unsigned long long; 
 
 // static constants
-static const u8 NUM_BITS_IN_BYTE = 8;
-static const u8 NUM_BITS_IN_WORD = 32;
+static const u8 BITS_IN_BYTE = 8;
+static const u8 BITS_IN_WORD = 32;
 
-static std::string stringifyHex(Word hex) {
+// help to print out hexadecimal numbers with zero padding
+static std::string stringifyHex(u64 hex, int width = 16) {
     std::stringstream stream;
-    stream << "0x" << std::setfill('0') << std::setw(8) << std::hex << hex;
+    stream << "0x" << std::setfill('0') << std::setw(width) << std::hex << hex;
     std::string result(stream.str());
     return result;
+}
+
+static std::string stringifyHex(u32 hex) {
+    return stringifyHex(hex,8);
 }
 
 static std::string stringifyHex(u16 hex) {
+    return stringifyHex(hex,4);
+}
+
+static std::string stringifyHex(u8 hex) {
+    return stringifyHex(hex,2);
+}
+
+
+// help to print out binary numbers with zero padding
+static std::string stringifyBin(u64 bin) {
     std::stringstream stream;
-    stream << "0x" << std::setfill('0') << std::setw(4) << std::hex << hex;
+    stream << "0b" << std::bitset<64>(bin);
     std::string result(stream.str());
     return result;
 }
 
-static std::string stringifyHex(u8 hex) {
+static std::string stringifyBin(u32 bin) {
     std::stringstream stream;
-    stream << "0x" << std::setfill('0') << std::setw(2) << std::hex << hex;
+    stream << "0b" << std::bitset<32>(bin);
+    std::string result(stream.str());
+    return result;
+}
+
+static std::string stringifyBin(u16 bin) {
+    std::stringstream stream;
+    stream << "0b" << std::bitset<16>(bin);
+    std::string result(stream.str());
+    return result;
+}
+
+static std::string stringifyBin(u8 bin) {
+    std::stringstream stream;
+    stream << "0b" << std::bitset<8>(bin);
     std::string result(stream.str());
     return result;
 }
