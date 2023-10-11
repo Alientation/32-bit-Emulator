1c30940560ba6b1744d69e45ce370cec60d4c913
Bill <mcpirate1760@gmail.com>
2023-10-11 11:20:32 -0500

    test converting stack pointer to address test pushing program counter to stack

--- tests/InternalTests/StackTest.cpp
+++ tests/InternalTests/StackTest.cpp
@@ -14,6 +14,17 @@ class StackTest : public testing::Test {
     }
 };
 
-TEST_F(StackTest, StackTest_Default) {
-    
+TEST_F(StackTest, StackTest_SPToAddress) {
+    ASSERT_EQ(cpu.SPToAddress(), 0x0001FFFF);
+    cpu.SP = 0x1234;
+    ASSERT_EQ(cpu.SPToAddress(), 0x00011234);
+    cpu.SP = 0x0000;
+    ASSERT_EQ(cpu.SPToAddress(), 0x00010000);
+}
+
+TEST_F(StackTest, StackTest_PushPCToStack) {
+    cpu.PC = 0x00012345;
+    cpu.pushPCToStack();
+    ASSERT_EQ(cpu.SP, 0x0000FFFB);
+    ASSERT_EQ(cpu.motherboard.ram.data[0x0001FFFC], 0x45);
 }
