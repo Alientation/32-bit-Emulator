74f02e5d5fbf2068f99ad006579f0951fbd72515
Bill <mcpirate1760@gmail.com>
2023-10-11 22:49:38 -0500

    more tests

---
+++ tests/InstructionTests/DecrementAndIncrementTests/DECTest.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include <AlienCPUTest.h>
+
+class DECTest : public testing::Test {
+    public: 
+        AlienCPU cpu;
+
+    virtual void SetUp() {
+        cpu.reset();
+    }
+
+    virtual void TearDown() {
+
+    }
+};
