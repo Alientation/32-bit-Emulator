e734ca774d8739184334c4da8b51c1946948a32b
Bill <mcpirate1760@gmail.com>
2023-10-11 22:32:52 -0500

    added stack instruction tests

--- tests/InstructionTests/Stack/StackTest.cpp
+++ tests/InstructionTests/Stack/StackTest.cpp
@@ -16,4 +16,17 @@ class StackTest : public testing::Test {
 };
 
 
-// TODO
+TEST_F(StackTest, PushAccumulatorImplied_Normal) {
+    cpu.writeWord(AlienCPU::POWER_ON_RESET_VECTOR, 0x00001023);
+    cpu.writeByte(0x00001023, AlienCPU::INS_PHA_IMPL);
+
+    cpu.A = 0x1234;
+
+    cpu.start(2);
+
+    EXPECT_EQ(cpu.motherboard.ram[0x0001FFFE], 0x34);
+    EXPECT_EQ(cpu.motherboard.ram[0x0001FFFF], 0x12);
+    EXPECT_EQ(cpu.SP, 0xFFFD);
+    EXPECT_EQ(cpu.cycles, 2);
+    EXPECT_EQ(cpu.PC, 0x00001024);
+}
