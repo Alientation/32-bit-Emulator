40cf74d779f91e4d62efe1dd4a5731c8992d5474
Bill <mcpirate1760@gmail.com>
2023-10-13 21:35:56 -0500

    fixed cmake misstyped source file

--- tests/AlienCPUTest.h
+++ tests/AlienCPUTest.h
@@ -3,7 +3,6 @@
 
 // CHEATY HACK to be able to access private members of another class (for testing purposes)
 #define private public
-
 #include <../src/Motherboard/AlienCPU.h>
 
 
---
+++ tests/TestUtil.h
@@ -0,0 +1,11 @@
+#ifndef TESTS_TESTUTIL_H_
+#define TESTS_TESTUTIL_H_
+
+#include <../src/Motherboard/AlienCPU.h>
+
+void LoadInstruction() {
+
+}
+
+
+#endif // TESTS_TESTUTIL_H_
