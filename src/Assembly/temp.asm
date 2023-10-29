e80490e39ec82ee6f825b060457f602144068fba
Bill <mcpirate1760@gmail.com>
2023-10-29 18:15:28 -0500

    fixed define bytes directives not named correctly

--- src/Assembly/temp.asm
+++ src/Assembly/temp.asm
@@ -2,10 +2,14 @@
 	Start of file
 *;
 
+.org		$1000
 loadlabel:
 		LDA	#$FFFF
 		LDA	#$1234
 .data
 .outfile	"test"
+datalabel:
+.db		$00,$01, $02, $03, $04, $05, $06, $07
+.d2b		$1234, $5678,
 
 ; End
