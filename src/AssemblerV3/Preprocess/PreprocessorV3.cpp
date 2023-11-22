97e9ed173e0f1293b1f8d30d92d73681f098acc0
Bill <billma.texas@gmail.com>
2023-11-22 14:32:02 -0600

    preprocessor

--- src/AssemblerV3/Preprocess/PreprocessorV3.cpp
+++ src/AssemblerV3/Preprocess/PreprocessorV3.cpp
@@ -1,6 +1,36 @@
 #include "PreprocessorV3.h"
 
 
-void preprocess(std::string filename) {
-	
+/**
+ * Constructs a preprocessor object with the given file.
+ * 
+ * @param file the file to preprocess
+ */
+Preprocessor::Preprocessor(File file) {
+	this->file = &file;
+
+	state = State::UNPROCESSED;
+}
+
+/**
+ * Destructs a preprocessor object
+ */
+Preprocessor::~Preprocessor() {
+	file->~File();
+}
+
+/**
+ * Preprocesses the file
+ */
+void Preprocessor::preprocess() {
+	state = State::PROCESSING;
+}
+
+/**
+ * Returns the state of the preprocessor
+ * 
+ * @return the state of the preprocessor
+ */
+Preprocessor::State Preprocessor::getState() {
+	return state;
 }
--- src/AssemblerV3/Preprocess/PreprocessorV3.h
+++ src/AssemblerV3/Preprocess/PreprocessorV3.h
@@ -1,7 +1,26 @@
+#include <../src/AssemblerV3/File.h>
+
 #include <string>
+
 #ifndef PREPROCESSORV3_H
 #define PREPROCESSORV3_H
 
-extern void preprocess(std::string filename);
+class Preprocessor;
+class Preprocessor {
+	public:
+		enum State {
+			UNPROCESSED, PROCESSING, PROCESSED_SUCCESS, PROCESSED_ERROR
+		};
+
+		Preprocessor(File file);
+		~Preprocessor();
+
+		void preprocess();
+		State getState();
+	private:
+		File* file;
+		State state;
+};
+
 
 #endif
