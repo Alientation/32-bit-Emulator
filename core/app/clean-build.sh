c35e2422b6e0da373350dfaeb52d91c0bd8c6855
Bill <billma.texas@gmail.com>
2024-07-15 12:26:31 -0500

    fixed build scripts

--- core/app/clean-build.sh
+++ core/app/clean-build.sh
@@ -3,8 +3,8 @@
 # This script cleans the build and install directories and then configures and builds the core application and its dependencies
 rm -rf ../assembler/build
 rm -rf ../assembler/install
-rm -rf ../emulator6502/build
-rm -rf ../emulator6502/install
+rm -rf ../emulator32bit/build
+rm -rf ../emulator32bit/install
 rm -rf ../util/build
 rm -rf ../util/install
 
--- core/emulator32bit/configure-and-install.bat
+++ core/emulator32bit/configure-and-install.bat
@@ -1,4 +1,4 @@
 @echo off
 
-cmake -B build -G Ninja -DCMAKE_INSTALL_PREFIX=install
+cmake -B build -G Ninja -DCMAKE_INSTALL_PREFIX="install"
 cmake --build build --target install
