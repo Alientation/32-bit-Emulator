f595def281bb2f5539b6fc1fc1193bcfda4ea88b
Bill <billma.texas@gmail.com>
2024-12-31 20:47:04 -0600

    added #define tests and fixed spacing
    Remove whitespace before and after a passed param to
    the #include preprocessor
    Added a string util function to replace all occurrences
    of a string

---
+++ core/assembler/test/preprocessor_test/define.cpp
@@ -0,0 +1,57 @@
+#include "assembler_test/assembler_test.h"
+
+TEST_F (EmulatorFixture, define_no_args)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/define_no_args.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 13);
+}
+
+TEST_F (EmulatorFixture, define_no_args_multiline)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/define_no_args_multiline.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 13);
+}
+
+TEST_F (EmulatorFixture, define_args)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/define_args.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 13);
+}
+
+TEST_F (EmulatorFixture, define_args_multiline)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/define_args_multiline.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 13);
+}
