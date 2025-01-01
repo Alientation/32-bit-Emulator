c40990f16decbd6a72038d6f81b47fca905e15e3
Bill <billma.texas@gmail.com>
2025-01-01 17:50:30 -0600

    Added rest of conditional preprocessor tests
    Future, rework expressions to be evaluatable (maybe)
    and add tests to ensure preprocessor throws errors when it should
    (add an exception that the preprocessor can throw and make errors more
    verbose)

--- core/assembler/test/preprocessor_test/conditional.cpp
+++ core/assembler/test/preprocessor_test/conditional.cpp
@@ -13,3 +13,76 @@ TEST_F (EmulatorFixture, conditional_ifdef)
 
 	ASSERT_EQ(machine->read_reg(0), 3);
 }
+
+TEST_F (EmulatorFixture, conditional_ifndef)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/conditional_ifndef.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 5);
+}
+
+TEST_F (EmulatorFixture, conditional_ifequ)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/conditional_ifequ.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 3);
+}
+
+TEST_F (EmulatorFixture, conditional_ifnequ)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/conditional_ifnequ.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 5);
+}
+
+TEST_F (EmulatorFixture, conditional_ifless_or_more)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/conditional_ifless_or_more.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 13);
+}
+
+TEST_F (EmulatorFixture, conditional_chain)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/conditional_chain.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 1);
+	ASSERT_EQ(machine->read_reg(1), 2);
+	ASSERT_EQ(machine->read_reg(2), 4);
+	ASSERT_EQ(machine->read_reg(3), 16);
+}
--- core/assembler/test/preprocessor_test/define.cpp
+++ core/assembler/test/preprocessor_test/define.cpp
@@ -55,3 +55,31 @@ TEST_F (EmulatorFixture, define_args_multiline)
 
 	ASSERT_EQ(machine->read_reg(0), 13);
 }
+
+TEST_F (EmulatorFixture, define_redefine)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/define_redefine.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 13);
+}
+
+TEST_F (EmulatorFixture, define_undefine)
+{
+	Process p ("-kp " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/src/define_undefine.basm "
+			"-outdir " + AEMU_PROJECT_ROOT_DIR +
+			"core/assembler/test/preprocessor_test/build");
+	ASSERT_TRUE (p.does_create_exe ());
+
+	LoadExecutable loader(*machine, p.get_exe_file());
+	machine->run(MAX_INSTRUCTIONS);
+
+	ASSERT_EQ(machine->read_reg(0), 13);
+}
