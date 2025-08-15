4faf97b02c614c50f2f1a5dc49d35ca48042d8d4
Bill <billma.texas@gmail.com>
2025-08-15 13:16:25 -0500

    Fine to leave the page constants where it is

--- core/emulator32bit/include/emulator32bit/emulator32bit_util.h
+++ core/emulator32bit/include/emulator32bit/emulator32bit_util.h
@@ -38,6 +38,5 @@ static inline constexpr T mask_0(T val, U8 bit_i, U8 len)
     return val & ~(((~U64(0)) >> (sizeof(U64) * 8 - len)) << bit_i);
 }
 
-/* TODO: This should be moved to some header file specific to the kernel. */
 static constexpr U8 kNumPageOffsetBits = 12;
 static constexpr U32 kPageSize = 1 << kNumPageOffsetBits;
